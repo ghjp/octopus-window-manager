d09b85db63b6db8fe4ac1ff2af47b5adee6aa532
Dr. Johann Pfefferl <pfefferl@gmx.net>
2013-11-22 21:29:00 +0100

    Do not grab the X server during desktop switch
    Instead we filter away some focus/enter events at the end of the whole
    switching process.

--- src/vdesk.c
+++ src/vdesk.c
@@ -15,7 +15,7 @@ static void _hide_vdesk_client(gpointer data, gpointer user_data)
   client_t *c = (client_t*)data;
   gswm_t *gsw = (gswm_t *)user_data;
   wa_hide(gsw, c);
-  //wframe_set_type(gsw, c->wframe, FALSE);
+  wframe_set_type(gsw, c->wframe, FALSE);
 }
 
 static void _unhide_vdesk_client(gpointer data, gpointer user_data)
@@ -46,9 +46,9 @@ void switch_vdesk(gswm_t *gsw, gint v)
   clist = scr->vdesk[scr->current_vdesk].clnt_list;
   TRACE("%s hide_clist=%d", __func__, g_list_length(clist));
   // Move the input focus away from any window client
-  XSetInputFocus(gsw->display, None, RevertToNone, CurrentTime);
-  XSync(gsw->display, False);
-  XGrabServer(gsw->display);
+  XSetInputFocus(gsw->display, None, RevertToPointerRoot, CurrentTime);
+  //XSync(gsw->display, False);
+  //XGrabServer(gsw->display);
   g_list_foreach(clist, _hide_vdesk_client, gsw);
 
 	scr->current_vdesk = v;
@@ -58,8 +58,16 @@ void switch_vdesk(gswm_t *gsw, gint v)
   TRACE("%s unhide_clist=%d", __func__, g_list_length(clist));
   g_list_foreach(clist, _unhide_vdesk_client, gsw);
   /*g_list_foreach(scr->sticky_list, _unhide_vdesk_client, gsw);*/
-  XSync(gsw->display, True); // Discard all events on the event queue
-  XUngrabServer(gsw->display);
+  {
+    XEvent ev;
+    gint ev_cnt = 0;
+    XSync(gsw->display, False);
+    while(XCheckMaskEvent(gsw->display, FocusChangeMask | EnterWindowMask | LeaveWindowMask, &ev))
+      ++ev_cnt;
+    TRACE("%s: ev_cnt=%d pending=%d", G_STRFUNC, ev_cnt, XPending(gsw->display));
+  }
+  //XSync(gsw->display, True); // Discard all events on the event queue
+  //XUngrabServer(gsw->display);
 
   if(old_focused_client)
     focus_client(gsw, old_focused_client, TRUE);
